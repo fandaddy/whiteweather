dc70976130e2c30bf3680b8cf5e39097b982b410
Jiang <jiangxiaobai1989@gmail.com>
2016-04-20 22:27:23 +0800

    First Commit.

---
+++ app/src/main/java/jiangxiaobai/com/whiteweather/MainActivity.java
@@ -0,0 +1,13 @@
+package jiangxiaobai.com.whiteweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
