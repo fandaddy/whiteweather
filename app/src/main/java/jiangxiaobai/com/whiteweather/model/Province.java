c3f993649a50835aa76ec068d2a5bec366edb6b3
Jiang <jiangxiaobai1989@gmail.com>
2016-04-21 10:24:41 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/jiangxiaobai/com/whiteweather/model/Province.java
@@ -0,0 +1,34 @@
+package jiangxiaobai.com.whiteweather.model;
+
+/**
+ * Created by JYH on 2016/4/21.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
